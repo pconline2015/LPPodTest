f50b9f7c00555e70d2a59b859ea075a535c992f2
HuangMinFeng <huangminfeng@huya.com>
2018-08-07 20:45:46 +0800

    initial Commit

---
+++ Example/Pods/Target Support Files/LPPodTest/LPPodTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LPPodTest : NSObject
+@end
+@implementation PodsDummy_LPPodTest
+@end
---
+++ Example/Pods/Target Support Files/Pods-LPPodTest_Tests/Pods-LPPodTest_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LPPodTest_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_LPPodTest_Tests
+@end
