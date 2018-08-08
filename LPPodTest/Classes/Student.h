4308eee559ea8a585159028dcb88f609ddcab1a0
HuangMinFeng <huangminfeng@huya.com>
2018-08-08 14:31:45 +0800

    移动Student路径

---
+++ LPPodTest/Classes/Student.h
@@ -0,0 +1,12 @@
+//
+//  Student.h
+//  LPPodTest
+//
+//  Created by HMFeng on 2018/8/8.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Student : NSObject
+
+@end
---
+++ LPPodTest/Classes/Student.m
@@ -0,0 +1,12 @@
+//
+//  Student.m
+//  LPPodTest
+//
+//  Created by HMFeng on 2018/8/8.
+//
+
+#import "Student.h"
+
+@implementation Student
+
+@end
